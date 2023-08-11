50c7f610f161caba22441a48b6c76d8ec1ab8905
peter <peter.arth5@gmail.com>
2023-08-11 10:56:24 +0100

    adjusting website and design content to look better

--- client/src/components/WebsiteInfo/WebsiteInfo.js
+++ client/src/components/WebsiteInfo/WebsiteInfo.js
@@ -5,20 +5,46 @@ const WebsiteInfo = () => {
     <div className="intro">
       <section className="about-me-intro">
         {" "}
-        <div className="center-img">
-          <img src={profilePicture} alt="a picture of Peter Arthur" />
+        <div className="image-alignment ">
+          <img
+            src={profilePicture}
+            className="image-size"
+            alt="a picture of Peter Arthur"
+          />
         </div>
-        <h2 className="center">Peter Arthur</h2>
-        <p className="section__subtitle--about center more-space">
-          Economics graduate turned React developer a
+        <p className="text-alignment section__subtitle--about text-alignment">
+          Peter Arthur
         </p>
-        <div className="self-description">
+        <p className="section__subtitle--about text-alignment more-space">
+          Economics graduate turned full stack hacker
+        </p>
+        <div className="text-alignment">
           <p className="self-text">
-            Working to grow as a front-end developer and also gain skill at core
-            algorithms and data structures.
+            This page is an experiment in real-time application development
+            using LLMs. It is an SPA built with React. The backend is built with
+            Express. It’s deployed on Heroku. The opponent is{" "}
+            <span className="line-through">GPT 4 Code Interpreter</span>{" "}
+            <span
+              title="I'm still waiting on GPT 4 access"
+              className="dotted-line"
+            >
+              GPT 3.5 Turbo
+            </span>
+            . I am still waiting for API access to GPT-4. Code Interpreter kicks
+            ass. I can’t wait for that to be opened up.{" "}
+            <span className="underline">
+              <a
+                href="https://github.com/prra-codes/peter-arthur-portfolio"
+                target="_blank"
+                className="portfolio-code-link"
+              >
+                Check out the code here
+              </a>
+            </span>
+            .
           </p>
         </div>
-        <ul className="social-list">
+        <ul className="social-list text-alignment">
           <li>
             <a href="mailto:prra@prra.codes" className="footer__link">
               {" "}
