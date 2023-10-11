e854b950cbd3106bd189f3393a50f89007abd4a3
peter <peter.arth5@gmail.com>
2023-10-11 11:19:06 +0100

    slight adjustment

--- server/server.js
+++ server/server.js
@@ -182,7 +182,6 @@ async function openAiSetPiece() {
   Yellow's move:`;
 
   const response = await openai.createChatCompletion({
-    // model: "gpt-3.5-turbo",
     model: "gpt-4-0613",
     messages: [
       {
